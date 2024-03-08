915f452b70dc9d24581523e6ac6a622e639fa50c
Mathew Titus <litsos.titus@gmail.com>
2024-03-08 12:09:48 -0800

    Fixing sun/moon position and color

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,4 +1,27 @@
 
+function makeToggle() {
+    // make link
+    let new_node = document.createElement("a")
+    new_node.setAttribute("id", "theme-toggle")
+    new_node.setAttribute("class", "theme-toggle")
+
+    // add icon
+    let thIcon = document.createElement("img")
+    thIcon.setAttribute("src", "/images/svg/sun.svg")
+    thIcon.setAttribute("alt", "sun icon")
+    thIcon.setAttribute("class", "theme-icon")
+    thIcon.setAttribute("style", "stroke:#babab; opacity:0.73; width: 26px; height: 26px; position: relative; top: 6px")
+    new_node.appendChild(thIcon);
+
+    // log
+    console.log("making new node: ")
+    console.log(new_node)
+
+    // add to page
+    let socials = document.querySelector(".ananke-socials");
+    socials.appendChild(new_node);
+}
+
 // theme color switch
 function setTheme(theme) {
     let body = document.body;
@@ -6,12 +29,12 @@ function setTheme(theme) {
     if (theme === "dark") {
         console.log("Going dark")
         body.classList.add("dark-mode");
-        themeIcon.src = "{{ .Site.BaseURL }}images/svg/moon.svg";
+        themeIcon.src = "/images/svg/moon.svg";
         themeIcon.alt = "moon icon";
     } else {
         console.log("Lightening")
         body.classList.remove("dark-mode");
-        themeIcon.src = "{{ .Site.BaseURL }}images/svg/sun.svg";
+        themeIcon.src = "/images/svg/sun.svg";
         themeIcon.alt = "sun icon";
     }
     // Save the user-selected theme mode to local storage.
@@ -20,6 +43,8 @@ function setTheme(theme) {
 
 // Retrieve the user-selected theme mode from local storage
 window.onload = (e) => {
+    // console.log("window loaded")
+    makeToggle();
     let theme = localStorage.getItem("theme") || "light";
     const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (isDarkMode) {
@@ -42,6 +67,4 @@ window.onload = (e) => {
         setTheme(theme);
     });
 }
-
-console.log("More text; static/js/theme-toggle.js")
 
