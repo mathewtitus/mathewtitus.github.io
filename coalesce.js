dc84ba1617bd1126a094fcd1a75fa667458c8de7
Mathew Titus <litsos.titus@gmail.com>
2024-06-22 19:02:12 -0700

    New production push

--- coalesce.js
+++ coalesce.js
@@ -203,7 +203,26 @@
   }
   window.addEventListener("load", setup);
   window.addEventListener("resize", resize);
-  document.getElementById("theme-toggle").addEventListener("click", function() {
-    changeMode();
+  function waitForElm(selector) {
+    return new Promise((resolve) => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+      const observer = new MutationObserver((mutations) => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+  waitForElm("#theme-toggle").then((elm) => {
+    elm.addEventListener("click", function() {
+      changeMode();
+    });
   });
 })();
--- theme-toggle.js
+++ theme-toggle.js
@@ -11,18 +11,23 @@
     new_node.appendChild(thIcon);
     let socials = document.querySelector(".ananke-socials");
     socials.appendChild(new_node);
+    return document.getElementById("theme-toggle");
   }
   function setTheme(theme) {
     let body = document.body;
+    var ico = document.querySelector(".dib img");
+    let img_path = ico.src;
     let themeIcon = document.querySelector(".theme-icon");
     if (theme === "dark") {
       body.classList.add("dark-mode");
       themeIcon.src = "/images/svg/moon.svg";
       themeIcon.alt = "moon icon";
+      ico.src = img_path.replace("icon.svg", "icon_dark.svg");
     } else {
       body.classList.remove("dark-mode");
       themeIcon.src = "/images/svg/sun.svg";
       themeIcon.alt = "sun icon";
+      ico.src = img_path.replace("icon_dark.svg", "icon.svg");
     }
     localStorage.setItem("theme", theme);
   }
@@ -30,7 +35,7 @@
     localStorage.setItem("transition", 0);
   }
   window.onload = (e) => {
-    makeToggle();
+    let toggle = makeToggle();
     let theme = localStorage.getItem("theme") || "light";
     const isDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
     if (isDarkMode) {
